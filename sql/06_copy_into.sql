f3a236742982dc02df8a48ab8d55013b0e9675ac
Benjamin Manguiat <105728442+benman17@users.noreply.github.com>
2025-09-08 13:14:05 -0400

    Update 06_copy_into.sql

--- sql/06_copy_into.sql
+++ sql/06_copy_into.sql
@@ -1,1 +1,10 @@
-
+CREATE OR REPLACE TABLE matches (
+    gameId STRING,
+    gameDuration FLOAT,
+    level INT,
+    lastRound INT,
+    placement INT,       -- 1–8
+    comp_score FLOAT,    -- from $6
+    champions VARIANT,   -- JSON parsed from $7
+    tier STRING          -- Platinum, Diamond, etc.
+);
